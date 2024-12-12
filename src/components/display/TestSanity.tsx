5aefe087fe194a06e3b1bebdcda3e50829b38dc1
Xavier Fenton <xavierfenton99@gmail.com>
2024-12-13 12:00:20 +1300

    get upload objects to show

--- src/components/display/TestSanity.tsx
+++ src/components/display/TestSanity.tsx
@@ -2,26 +2,50 @@ import React, { use, useMemo, useState } from 'react';
 import { retrieveContent } from '../../../sanity/lib/utils/fetchPosts';
 import { client } from '../../../sanity/lib/utils/sanityClient';
 import { WorkBatch } from 'sanity/lib/types/work';
+import { buildFilePath } from "@sanity/asset-utils"
+import { dataset, projectId, apiVersion } from '../../../sanity/env';
+import PreviewObject from './PreviewObject';
+
+type ConfigType = {
+    projectId: string,
+    dataset: string,
+    apiVersion: string
+}
+
+
+const builderConfig = { projectId: projectId, dataset: dataset, apiVersion: apiVersion }
+
+/* 
+    Url Builder for getting GLB files from CDN
+    'https://apicdn.sanity.io/files/<projectId>/<dataset>/<file_src>'
+*/
+function createFilePath(config: ConfigType, src: string) {
+
+    const newSrc = src.replace('file-', '').replace('-glb', '.glb')
+    return `https://apicdn.sanity.io/files/${config.projectId}/${config.dataset}/${newSrc}`
+}
+
+const urlBuilder = (src: string): string => {
+    const url = createFilePath(builderConfig, src)
+    return url
+}
+
 
 const TestSanity = () => {
     const [data, setData] = useState<WorkBatch>([])
 
     useMemo(async () => {
         const data = await retrieveContent(client)
-
         return setData(data)
     }, [])
 
     return (
         <div className='h-dvh'>
-            hello
-            <ul>
-                {data && data.map((post) => (
-                    <li key={post._id}>
-                        <a>{post?.title}</a>
-                    </li>
+            <div className='h-full'>
+                {data && data.map((post, index) => (
+                    <PreviewObject key={index} value={urlBuilder(post.objectFile.asset._ref)} />
                 ))}
-            </ul>
+            </div>
         </div >
     );
 };
