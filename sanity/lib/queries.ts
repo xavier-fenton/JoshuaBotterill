3772b8d6855d6d8f1865c48f49e92e1536ba247d
Xavier Fenton <xavierfenton99@gmail.com>
2024-12-13 11:50:28 +1300

    Specify queries

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,3 +1,5 @@
 import { defineQuery } from 'next-sanity'
 
-export const WORKS_QUERY = defineQuery(`*[_type == "works"]`)
+export const WORKS_QUERY = defineQuery(`*[_type == "works"]{_id, title, objectFile}`)
+
+export const FILES_QUERY = defineQuery(`*[_type == "sanity.fileAsset"]`)
