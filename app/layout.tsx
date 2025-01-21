f259c586af40785e29b7925b8aac79fbb346456a
Xavier Fenton <xavierfenton99@gmail.com>
2025-01-21 13:45:04 +1300

    Dont need

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Layout } from '@/components/dom/Layout'
 import '@/global.css'
-import DataProvider from './provider'
+// import DataProvider from './provider'
 
 export const metadata = {
   title: 'Joshua Botterill',
@@ -17,9 +17,7 @@ export default function RootLayout({ children }) {
       <head />
       <body>
         {/* To avoid FOUT with styled-components wrap Layout with StyledComponentsRegistry https://beta.nextjs.org/docs/styling/css-in-js#styled-components */}
-        <DataProvider>
           <Layout>{children}</Layout>
-        </DataProvider>
       </body>
     </html>
   )
